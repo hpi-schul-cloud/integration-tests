12f44c403ddc654cd7703e29908922974f065cd3
yuliashikh <yulia.shikhareva@gmail.com>
2019-12-16 10:59:33 +0100

    Update schoolLogo.js

--- page-objects/schoolLogo.js
+++ page-objects/schoolLogo.js
@@ -1,23 +1,28 @@
 'use strict';
 //const pathDirectory = require('../runtime/img');
 const path = require('path');
+let remoteFilePath;
 
 module.exports = {
   verifyUpload: async function() {
+    await driver.reloadPage()
+    let selector = await driver.$('#preview-logo');
+    let src = await selector.getAttribute('src');
+
+   // let src = await selector.
+
       
 
   },
   uploadSchoolLogo: async function() {
-      //let uploadBtn = await driver.$('[data-testid="upload_load_btn"]');
-      //await uploadBtn.click();
-      const filePath = await path.join(__dirname, '../runtime/img/logo-logo-schwarz-100_760x340.png');
-      const remoteFilePath = await driver.uploadFile(filePath);
-    //   await driver.$('#logo-input').setValue(remoteFilePath);
-    //   let uploadElement = await driver.$('#logo-input');
-    //   await uploadElement.sendKeys('../runtime/img');
+   
+    const filePath = path.join(__dirname, '../runtime/img/logo-logo-schwarz-100_760x340.png');
 
-    let logoFileInput = await driver.$('#logo-input');
-    await logoFileInput.setValue(remoteFilePath);
+   // const remoteFilePath = await driver.uploadFile(filePath);
+    let uploadElement = await driver.$('#logo-input');
+    await driver.attachFile(uploadElement, filePath);
+    //await uploadElement.chooseFile(uploadElement, filePath);
+   // await uploadElement.setValue(remoteFilePath);
     await driver.pause(1500);
     let container = await driver.$('.edit-form-school');
     let submitBtn = await container.$('button[type="submit"]');
